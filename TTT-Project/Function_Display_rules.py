0ce23f268ff76afc366e0095f1b28abbbc9a7423
suricate2828 <jo.suri2828@gmail.com>
2024-11-07 13:41:56 +0100

    changement display rules

--- TTT-Project/Function_Display_rules.py
+++ TTT-Project/Function_Display_rules.py
@@ -1,19 +1,14 @@
-
+from Functions_board import board, Display_board
 def Display_rules(y_n):
     #affiche ou non les règles du jeu, l'assignation des symboles et le
     # système de manches
     if y_n in ('o', 'oui', 'O', 'Oui', 'y', 'yes', 'Y', 'Yes'):
-        print()
-        print(f"| 1 | 2 | 3 |" )
-        print("-"*13)
-        print(f"| 4 | 5 | 6 |" )
-        print("-"*13)
-        print(f"| 7 | 8 | 9 |" )
-        print()
         print("Chaque joueur choisi son symbole pour la partie.\
               A tour de rôle les joueurs vont placer leur symbole dans la grille de jeu.\
               Le premier à aligner ses trois symboles, horizontalement, verticalement, ou diagonalement, gagne la partie.\
               Si aucun joueur n'aligne ses symboles, c'est match nul et nous t'invitons à rejouer !")
+        print()
+        Display_board
         print()
         return
     else :
