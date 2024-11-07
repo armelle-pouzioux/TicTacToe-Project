ac62613ef2a636ce05b7551839936620c3d1b1d8
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2024-11-07 13:06:11 +0100

    update pour synchro

--- TTT-Project/Fichier_main.py
+++ TTT-Project/Fichier_main.py
@@ -2,14 +2,6 @@ board = [coordinates for coordinates in range(1,10)]
     #cette liste à répéter à chaque début de manche définit / réinitialise la
     # grille, générant 9 "coordinates" vierges
 
-symbol = ['x','o','&','#','★','♥']
-    #cette liste établit les différents symboles pouvant être utilisés pour
-    # représenter le jeu de chaque joueur
-
-players = [' ', ' ']
-    #cette liste énumère et désigne le ou les deux joueurs participants aux
-    # différentes manches
-
 TURN = 0
     #cette variable compte le nombre de tours écoulés depuis le début de la
     # manche, permettant d'arrêter celle-ci lorsque les 9 tours sont écoulés
@@ -19,9 +11,7 @@ SYMBOL_J2 = ''
 SCORE_J1 = ''
 SCORE_J2 = ''
 
-
-from Function-Display_rules import Display_rules
-
+from Function_Display_rules import Display_rules
     # CODE MAIN
     #compile et initie l'intégralité des algorythmes composant le jeu,
     # permettant la délimitation de manches
@@ -31,16 +21,19 @@ print("Le premier joueur choisit le symbole qui le représentera pendant la\
  manche,")
 print("Ce symbole doit être désigné à partir de la liste qui suit :")
 
-from Function-Choose import Choose_symbol
+from Function_Choose_symbol import Choose_symbol
 
 Choose_symbol(0)
 print("Le deuxième joueur choisit également son symbole conformément à la\
  liste")
 Choose_symbol(1)
+from Function_Choose_symbol import players
 print("Le joueur 1 jouera donc avec le symbole :", players[0],\
  "et le joueur 2 :", players[1])
 SYMBOL_J1 = players[0]
 SYMBOL_J2 = players[1]
+print(SYMBOL_J1)
+print(SYMBOL_J2)
 
 #from Fonctions_Rounds import Launch_turn, TRY_coor, Check_win
 #from Fonctions_Display import Reset_board, Display_board
