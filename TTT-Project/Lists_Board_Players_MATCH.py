7f8fdaca092d9fa4e37bceb54fbe375bf7d51be5
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2024-11-07 18:25:29 +0100

    COMMIT FONCTIONNEL

--- TTT-Project/Lists_Board_Players_MATCH.py
+++ TTT-Project/Lists_Board_Players_MATCH.py
@@ -1,4 +1,4 @@
-board = [coordinates for coordinates in range(1,10)]
+board = [' ' for coordinates in range(1,10)]
     #cette liste à répéter à chaque début de manche définit / réinitialise la
     # grille, générant 9 "coordinates" vierges
 
@@ -6,6 +6,5 @@ players = ['','']
     #cette liste permet de référer à l'un ou l'autre signe représentant le
     # joueur 1 ou 2 ([O:1])
 
-MATCH = 1
-SCORE_J1 = 0
-SCORE_J2 = 0
+SCORES = [0,0]
+MATCH = [1]
