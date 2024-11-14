7e5d0148c3352d8c1d56f48abd9f09f176a528ae
joseph-mangeot <joseph.mangeot@laplateforme.io>
2024-11-15 00:48:00 +0100

    Update and rename Function_Display_rules.py to function_display_rules.py

--- TTT-Project/function_display_rules.py
+++ TTT-Project/function_display_rules.py
@@ -1,7 +1,7 @@
-def Display_rules(y_n):
+def display_rules(y_n):
     #affiche ou non les règles du jeu, l'assignation des symboles et le
     # système de manches
-    from Functions_board import Display_board
+    from functions_board import display_board
     if y_n in ('o', 'oui', 'O', 'Oui', 'y', 'yes', 'Y', 'Yes'):
         print("\nChaque joueur choisi son symbole pour la partie.\
  A tour de rôle les joueurs vont placer leur symbole dans la grille de jeu.\n\
@@ -9,7 +9,7 @@ def Display_rules(y_n):
  diagonalement, gagne la partie.\n Si aucun joueur n'aligne ses symboles, c'est\
  match nul et nous t'invitons à rejouer !")
         print()
-        Display_board()
+        display_board()
         print()
         return
     else :
