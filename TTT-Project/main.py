98036efa0ec4e960454bc8061192dd93bc79ad70
joseph-mangeot <jolyne.mangeot@gmail.com>
2024-11-15 00:51:05 +0100

    Update main.py

--- TTT-Project/main.py
+++ TTT-Project/main.py
@@ -1,31 +1,31 @@
 #   compile et initie l'intégralité des algorythmes composant le jeu,
 #    permettant la délimitation de manches
 
-from Function_Display_rules import Display_rules
-Display_rules(input("Bienvenue au tic-tac-toe ! Voulez-vous visualiser les\
+from function_display_rules import display_rules
+display_rules(input("Bienvenue au tic-tac-toe ! Voulez-vous visualiser les\
  règles avant de commencer ?\n   (affichage de la grille des coordonnées) o/n :\n   "))
 
-from Function_Choose_symbol import Choose_symbol
+from function_choose_symbol import choose_symbol
 print(f"Le premier joueur choisit le symbole qui le représentera pendant la\
  manche,\nce symbole doit être désigné à partir de la liste qui suit : \n")
-Choose_symbol(0)
+choose_symbol(0)
 print("\nLe deuxième joueur choisit également son symbole conformément à la\
  liste\n")
-Choose_symbol(1)
+choose_symbol(1)
 
 from Lists_Board_Players_MATCH import players
 print("Le joueur 1 jouera donc avec le symbole :", players[0],\
  "et le joueur 2 :", players[1])
 
-from Functions_Rounds import Launch_match
+from functions_rounds import launch_match
 while True:
-    from Lists_Board_Players_MATCH import MATCH, SCORES
+    from lists import MATCH, SCORES
     print(f"\n Début du match {MATCH[0]} ! \n     Score : {SCORES[0]} {players[0]} - {SCORES[1]} {players[1]}\n")
-    print(Launch_match())
+    print(launch_match())
     replay = input("Souhaitez-vous rejouer ? o/n :\n   ")
     if replay in ("o","O","oui","Oui","y","Y","yes","Yes"):
         continue
     else:
-        from Lists_Board_Players_MATCH import SCORES
+        from lists import SCORES
         print(f"\nMerci d'avoir joué, à bientôt !\n Scores finaux : {SCORES[0]} {players[0]} - {SCORES[1]} {players[1]}\n")
         break
