4f3d5a790a85466e58ec7951886eeebcb183fb53
joseph-mangeot <jolyne.mangeot@gmail.com>
2024-11-15 00:23:17 +0100

    final for presentation

--- TTT-Project/Functions_Display.py
+++ TTT-Project/Functions_Display.py
@@ -1,11 +1,10 @@
 def reset_board():
     # reset the board list to spaces not to create conflict between matches
-    from settings import board
-    board[0:9] = [' ' for coordinates in range(1,10)]
+    import settings; settings.board[0:9] = [' ' for coordinates in range(0,9)]
 
 
 def display_board():
-    # print said gameboard with updated board list to keep settings.players aware of
+    # print said gameboard with updated board list to keep players aware of
     # their in-game moves
     from settings import board
     print(f"\n| {board[0]} | {board[1]} | {board[2]} |          | 1 | 2 | 3 |")
@@ -28,7 +27,6 @@ def display_rules():
     response = input("\nBienvenue au tic-tac-toe ! Voulez-vous visualiser les\
  règles avant de commencer ?\n  (affichage de la grille des coordonnées)\
  o/n :\n   ")
-
     if response in ('o', 'oui', 'O', 'Oui', 'y', 'yes', 'Y', 'Yes'):
         print("\nChaque joueur choisit son symbole pour la partie.\n")
         time.sleep(2.5)
@@ -38,5 +36,4 @@ verticalement,\nou diagonalement, gagne la partie. Si aucun joueur n'aligne \
 ses symboles,\nc'est match nul et nous t'invitons à rejouer !\n")
         time.sleep(7) ; display_board()
         print("\nVoici la grille de jeu et ses 9 cases.\n") ; time.sleep(3)
-    else :
-        print("\nBonne chance !\n") ; time.sleep(1.5)
+    else : print("\nBonne chance !\n") ; time.sleep(1.5)
